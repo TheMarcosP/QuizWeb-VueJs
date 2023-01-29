3c52b05b7245790039ae129ca4aab3415e4ef336
TheMarcosP <50151245+TheMarcosP@users.noreply.github.com>
2023-01-28 23:52:51 -0300

    start

---
+++ src/components/QuizPreview.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    header: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: String,
+        required: true
+    },
+    nQuestions: {
+        type: String,
+        required: true
+    }
+})
+
+
+</script>
+
+<template>
+    <main>
+        <div>
+            <img :src="'src/data/images/math.webp'" alt="quiz image" />
+            <h2>{{ header }}</h2>
+            <h3>{{ nQuestions }} questions</h3>
+        </div>
+        
+    </main>
+</template>
+
+<style scoped>
+img{
+    width: 100%;
+}
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import {ref, computed} from 'vue'
+import q from '../data/quizes.json'
+import QuizPreview from '../components/QuizPreview.vue';
+const quizes = ref(q)
+
+</script>
+
+<template>
+  <main>
+    <header>
+      <h1>Welcome to Quizes</h1>
+      <input  placeholder="search" />
+    </header>
+    <ul>
+      <li v-for="quiz in quizes" :key="quiz.id">
+        <QuizPreview :header="quiz.header" img="{{ quiz.img }}" :nQuestions="quiz.questions.length" />
+      </li>
+    </ul>
+</main>
+</template>
+
+
+<style scoped>
+header{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1em;
+  background-color: rgb(31, 31, 31);
+  color: white;
+  box-shadow: 0 0 10px 0 rgba(0,0,0,0.2);
+  margin: 1em;
+  border-radius: 10px;
+}
+
+input{
+  padding: 0.5em;
+  margin-right: 3em;
+  border: none;
+  border-radius: 10px;
+  outline: none;
+}
+
+
+ul{
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1em
+
+}
+
+li{
+  list-style: none;
+  text-align: center;
+  padding: 0.5em;
+  
+  width: 100%;
+
+  /* border: 1px solid black; */
+  /* border-radius: 10px; */
+  background-color: rgb(31, 31, 31);
+  box-shadow: 0 0 10px 0 rgba(0,0,0,0.2);
+  
+}
+
+</style>
